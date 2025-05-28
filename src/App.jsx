f65e8a3737704560bdf38c45df150ded185062ca
Mohamed Elsayed <moelsayed524@gmail.com>
2025-05-28 11:09:04 +0300

    remove preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import Home from "./sections/Home";
-import { useEffect, useState } from "react";
-import Preloader from "./Preloader/Preloader";
 import ThemeProvider from "./providers/ThemeProvider";
 import ThemesBox from "./sections/ThemesBox";
 import About from "./sections/About";
@@ -11,45 +9,31 @@ import Footer from "./sections/Footer";
 import { AnimatedSection } from "./Components/AnimatedSection";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const loaderTimer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => {
-      clearTimeout(loaderTimer);
-    };
-  }, []);
   return (
     <main className="min-h-screen relative">
-      {loading ? (
-        <Preloader />
-      ) : (
-        <ThemeProvider>
-          <div
-            id="portfolio"
-            className="section-style snap-section min-h-screen scroll-smooth"
-          >
-            <div className="container-hero flex flex-row justify-between max-lg:flex-col overflow-auto  h-screen  scroll-smooth">
-              <div className="lg:basis-[45%] lg:sticky lg:top-0 max-h-screen ">
-                <Home />
-              </div>
-              <div className="lg:basis-[52%] h-fit flex flex-col gap-[10px] pl-2 ">
-                <AnimatedSection>
-                  <About />
-                </AnimatedSection>
-                <Skills />
-                <Projects />
-                <Contact />
-                <Footer />
-              </div>
+      <ThemeProvider>
+        <div
+          id="portfolio"
+          className="section-style snap-section min-h-screen scroll-smooth"
+        >
+          <div className="container-hero flex flex-row justify-between max-lg:flex-col overflow-auto  h-screen  scroll-smooth">
+            <div className="lg:basis-[45%] lg:sticky lg:top-0 max-h-screen ">
+              <Home />
+            </div>
+            <div className="lg:basis-[52%] h-fit flex flex-col gap-[10px] pl-2 ">
+              <AnimatedSection>
+                <About />
+              </AnimatedSection>
+              <Skills />
+              <Projects />
+              <Contact />
+              <Footer />
             </div>
           </div>
-          <ThemesBox />
-        </ThemeProvider>
-      )}
+        </div>
+
+        <ThemesBox />
+      </ThemeProvider>
     </main>
   );
 }
